22d873247db7325d9e56a7359b90475df7bc1c33
nontajid <me@nontajid.com>
2018-10-05 00:08:22 +0700

    feat/log message with color

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ require('dotenv').config();
 const CloudFlare = require('./src/ddns-cloudflare');
 const MyIp = require('./src/my-ip');
 
-const noOfRetry = 5;
-const timeBeforeRetry = 10000;
-const updateInterval = 15000;
+const noOfRetry = 20;
+const timeBeforeRetry = 20000;
+const updateInterval = 30000;
+
+const colorLog = (message, color) => {
+    const logColor = {    
+        reset : "\x1b[0m",
+        black : "\x1b[30m",
+        red : "\x1b[31m",
+        green : "\x1b[32m",
+        yellow : "\x1b[33m",
+        blue : "\x1b[34m",
+        magenta : "\x1b[35m",
+        cyan : "\x1b[36m",
+        white : "\x1b[37m"
+    }
+    color = color? color in logColor? logColor[color] : logColor.black : logColor.black;
+    return message? console.log(color, message , logColor.reset ) : null;
+}
+
 class Main {
     constructor() {
-        console.log('Welcome To DDNS Updater We are starting now....');
+        colorLog('Welcome To DDNS Updater We are starting now....','green');
         this.email = process.env.CLOUD_FLARE_EMAIL;
         this.apiKey = process.env.CLOUD_FLARE_KEY;
         this.mainDomain = process.env.MAIN_DOMAIN;
@@ -25,10 +42,10 @@ class Main {
 
         this.myIp.getIp()
         .then( () => {
-            console.log('Connected To Ip Server');
+            colorLog('Local Ip is retrived', 'yellow');
             this.initUpdater(); //Get Initial Ip StartUpdater
         }).catch( error => {
-            console.log(error);
+            console.error(error);
             this.initRetry++;
             if(this.initRetry < noOfRetry) setTimeout(this.init.bind(this), timeBeforeRetry);
         });
@@ -37,11 +54,11 @@ class Main {
     initUpdater() {
         this.cloudFlare.init()
         .then( zone => {
-            console.log('Initailize Updater Done');
+            colorLog('Initailize Cloudflare Updater Done','yellow');
             this.cloudFlare.setMainDomain(this.mainDomain);
             setInterval(this.checkAndSetNewIP.bind(this), updateInterval);
         }).catch( error => {
-            console.log(error);
+            console.error(error);
             this.initRetry++;
             if(this.initRetry < noOfRetry) setTimeout(this.initUpdater.bind(this), timeBeforeRetry);
         });
@@ -53,10 +70,10 @@ class Main {
             let ip = this.myIp.currentIp;
             if(record.content != ip) {
                 this.cloudFlare.updateRecord(ip, this.domain, this.recordType, record.id)
-                .then( () => console.log(`updater Ip to ${ip}`))
-                .catch( e => console.log(e));
+                .then( () => colorLog(`Ip is updated to ${ip}`,'blue'))
+                .catch( e => console.error(e));
             }
-        }).catch( e => console.log(e));
+        }).catch( e => console.error(e));
     }
 }
 
